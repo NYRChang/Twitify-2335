98a25cd5d8f24e00f7ce138833c6d7d6b9554cf9
Richard R. Chang <65420925+NYRChang@users.noreply.github.com>
2020-06-25 18:18:22 -0400

    merge
    merge

--- app/twitify.py
+++ app/twitify.py
@@ -1,8 +1,34 @@
 #app/twitify.py
 
+import sys
+import csv
+import os
 from dotenv import load_dotenv
 import requests
+import spotipy
+import spotipy.util as util
 
-print("Hello")
+user_id = os.environ.get("spotify_username")
+playlist_scope = os.environ.get("scope")
+spotify_client_id = os.environ.get("client_id")
+spotify_client_secret = os.environ.get("client_secret")
 
-#test
+
+
+#https://spotipy.readthedocs.io/en/2.12.0/
+
+# Returns a spotify client object
+def init_spotify_client():
+    try:
+        print('Initialising Spotify Client....')
+        token = util.prompt_for_user_token(user_id, playlist_scope,
+                                           client_id=spotify_client_id,
+                                           client_secret=spotify_client_secret,
+                                           redirect_uri='http://localhost/')
+        spotify_client = spotipy.Spotify(auth=token)
+        print('\nClient initialised!\n')
+        return spotify_client
+    except:
+        sys('\nError initialising Spotify Client!\n')
+
+spotify_client = init_spotify_client()
