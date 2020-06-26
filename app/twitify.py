b13c1b5e27dd365209a3d428e91b0646991c9120
Richard R. Chang <65420925+NYRChang@users.noreply.github.com>
2020-06-25 20:10:03 -0400

    Successfully add song
    Successfully add song to playlist (hard-code)

--- app/twitify.py
+++ app/twitify.py
@@ -17,10 +17,17 @@ import spotipy.util as util
 #playlist_id = sys.argv[2]
 #track_ids = sys.argv[3:]
 
+#https://github.com/nikhilgaba001/YoutubeToSpotify/blob/master/YoutubeToSpotify.py
+#https://stackoverflow.com/questions/49418717/spotify-api-illegal-redirect-uri
+
+#Validating Spotify Connection 
+
 SPOTIFY_USERNAME = 'ssjmusashi'
 CLIENT_ID = 'bb23555d00c74343851e1bd782fd1731'
 CLIENT_SECRET = 'bf206e3b6d604b7b88d3afd9e739ec0f'
 SCOPE = 'playlist-modify-private'
+playlist_id = '5yeB2JFf09vQ6Na9003kMo'
+track_ids = ["1lKS7SZQ7r5vOChLwJurmm"]
 
 def init_spotify_client():
     try:
@@ -36,6 +43,10 @@ def init_spotify_client():
         sys('\nError initialising Spotify Client!\n')
 
 spotify_client = init_spotify_client()
+
+spotify_client.trace = False
+results = spotify_client.user_playlist_add_tracks(SPOTIFY_USERNAME, playlist_id, track_ids)
+print(results)
 
 #scope = 'playlist-modify-public'
 #token = util.prompt_for_user_token(username, scope)
