c5a8b9f1ecc1a584001e501b353d695205ff1542
Richard R. Chang <65420925+NYRChang@users.noreply.github.com>
2020-06-25 21:37:07 -0400

    obtain token info
    obtain token info

--- app/twitify.py
+++ app/twitify.py
@@ -22,13 +22,14 @@ playlist_id = os.environ.get('playlist')
 SCOPE = 'playlist-modify-private'
 track_ids = ["1lKS7SZQ7r5vOChLwJurmm","13X42np3KJr0o2LkK1MG76"]
 
-def init_spotify_client():
-    try:
-        print('Initialising Spotify Client....')
-        token = util.prompt_for_user_token(SPOTIFY_USERNAME, SCOPE,
+token = util.prompt_for_user_token(SPOTIFY_USERNAME, SCOPE,
                                            client_id=CLIENT_ID,
                                            client_secret=CLIENT_SECRET,
                                            redirect_uri='http://localhost:8888/callback/')
+
+def init_spotify_client():
+    try:
+        print('Initialising Spotify Client....')
         spotify_client = spotipy.Spotify(auth=token)
         print('Spotify Connection Successful!')
         return spotify_client
@@ -36,22 +37,10 @@ def init_spotify_client():
         sys('Spotify Connection Failed')
 
 spotify_client = init_spotify_client()
-breakpoint()
-
-
-
 
 #Step 1: Pull data from the twitter account
 
 #Step 2: Search spotify for songs (https://developer.spotify.com/console/get-search-item/)
-
-""" def get_spotify_uri(song, artist):
-    query = "https://api.spotify.com/v1/search?q={}%20{}&type=track%2Cartist&market=US&limit=10&offset=5".format(song,artist)
-    response = requests.get(query, headers={"Content-Type": "application/json","Authorization: Bearer {}".format(token)})
-    response_json = response.json()
-    return response_json["tracks"]["items"][0]["uri"]
-
-get_spotify_uri(My Love, Justin Timberlake) """
 
 #Step 3: Create a new playlist (https://developer.spotify.com/console/post-playlists/)
 
